9d0caba6fb8e84bab125ce407b9bd5ebe680f9b1
Rafael Peretta Santos <peretta3x@gmail.com>
2012-10-29 23:54:03 -0200

    Criado controller de Project.

---
+++ resources/js/app/controller/Project.js
@@ -0,0 +1,56 @@
+Ext.define('ScrumTool.controller.Project', {
+	extend: 'Ext.app.Controller',
+	
+	stores: ['Projects'],
+	
+	init: function() {
+		this.control({
+			'projectgrid': {
+				editproject: this.onEditProject,
+				removeproject: this.onRemoveProject
+			},
+			
+			'projectgrid > toolbar > button[action=newProject]': {
+				click: this.onNewProject
+			},
+			
+			'editproject > toolbar > button[action=save]': {
+				click: this.onSaveProject
+			},
+		});
+	},
+	
+	onNewProject: function() {
+		var projectWindow = Ext.widget('editproject', {title: 'Novo Projeto'});
+		projectWindow.show();
+	},
+	
+	onSaveProject: function(button) {
+		var window = button.up('window'),
+        form   = window.down('form'),
+        record = form.getRecord(),
+        values = form.getValues();
+	
+        if (record == null) {
+        	record = Ext.create('ScrumTool.model.Project');
+        	record.set(values);
+        	this.getProjectsStore().add(record.data);        	
+        }  else {        	
+        	record.set(values);
+        }
+       		
+		window.close();
+	},
+	
+	onEditProject: function(grid, rowIndex, colIndex) {
+		var projectWindow = Ext.widget('editproject', {title: 'Editar Projeto'});
+		projectWindow.show();
+		var record = grid.getStore().getAt(rowIndex);
+		projectWindow.down('form').loadRecord(record);
+	},
+	
+	onRemoveProject: function(grid, rowIndex, colIndex) {
+		console.log(rowIndex);
+		grid.getStore().removeAt(rowIndex);
+	}
+});
