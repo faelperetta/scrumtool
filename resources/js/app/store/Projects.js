bb76a42daf8271382566aeb699240de3e0a444d2
Rafael Peretta Santos <peretta3x@gmail.com>
2012-11-06 00:02:46 -0200

    Adicionado proxy na store para obter os dados do servidor.

--- resources/js/app/store/Projects.js
+++ resources/js/app/store/Projects.js
@@ -1,7 +1,29 @@
 Ext.define('ScrumTool.store.Projects', {
 	extend: 'Ext.data.Store',
 	requires: 'ScrumTool.model.Project',
-	model: 'ScrumTool.model.Project'
+	model: 'ScrumTool.model.Project',
 		
+	proxy: {
+		type: 'ajax',
+		method: 'post',
 		
-});
+		api: {
+			create: 'projects/save',
+			read: 'projects/all'
+		},
+		
+		reader: {
+			type: 'json',
+			root: 'result',
+			successProperty: 'success'
+		},
+		
+		writer: {
+		    writeAllFields: true,
+		    encode: true,
+		    root: 'data',
+		}
+	},
+	
+	autoLoad: true
+})
