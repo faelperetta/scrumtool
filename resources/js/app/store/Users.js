9ff19e29652af2ef5eec8ea3f1318f37df755d07
Rafael Peretta Santos <peretta3x@gmail.com>
2012-11-07 00:36:28 -0200

    Criada a store de Users.

---
+++ resources/js/app/store/Users.js
@@ -0,0 +1,28 @@
+Ext.define('ScrumTool.store.Users', {
+	extend: 'Ext.data.Store',
+	requires: 'ScrumTool.model.User',
+	model: 'ScrumTool.model.User',
+	
+	proxy: {
+		type: 'ajax',
+		method: 'post',
+		
+		api: {
+			create: 'users/save',
+			read: 'users/all',
+			update: 'users/save'
+		},
+		
+		reader: {
+			type: 'json',
+			root: 'result',
+			successProperty: 'success'
+		},
+		
+		writer: {
+		    writeAllFields: true,
+		    encode: true,
+		    root: 'data',
+		}
+	}
+});
