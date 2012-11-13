90c6ca9096d9131d07a5aeb367f602eba13e774a
Rafael Peretta Santos <peretta3x@gmail.com>
2012-11-12 23:06:58 -0200

    Adicionada a lib DateUtil para ser carregada no autoload.

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('doctrine', 'utils', 'session');
+$autoload['libraries'] = array('doctrine', 'utils', 'session', 'dateutil');
 
 
 /*
