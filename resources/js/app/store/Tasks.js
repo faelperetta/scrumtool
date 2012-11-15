cfdacfe44a79e10bd0618e76b4afdb0e93be2c98
Rafael Peretta Santos <peretta3x@gmail.com>
2012-11-15 00:49:24 -0200

    Configurada as urls para salvar, editar e listar as tarefas.

--- resources/js/app/store/Tasks.js
+++ resources/js/app/store/Tasks.js
@@ -3,13 +3,27 @@ Ext.define('ScrumTool.store.Tasks', {
 	requires: 'ScrumTool.model.Task',
 	model: 'ScrumTool.model.Task',
 	
-	groupField: 'storyName'
+	groupField: 'storyName',
 	
-	/* data : [
-	         {id: 1, storyName: 'Story 1', description: 'Spencer'},
-	         {id: 2, storyName: 'Story 1', description: 'Maintz'},
-	         {id: 3, storyName: 'Story 2', description: 'Conran'},
-	         {id: 4, storyName: 'Story 2', description: 'Avins'},
-	         {storyName: 'Story 3', description: null},
-	     ]*/
+	proxy: {
+		type: 'ajax',
+		
+		api: {
+			create: 'tasks/save',
+			read: 'tasks/all',
+			update: 'tasks/save'
+		},
+
+		reader: {
+			type: 'json',
+			root: 'result',
+			successProperty: 'success'
+		},
+		
+		writer: {
+		    writeAllFields: true,
+		    encode: true,
+		    root: 'data',
+		}
+	}
 });
