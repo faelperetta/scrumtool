42677cdc938a5acc4a0fc285624b47bdeff3ae1f
Rafael Peretta Santos <peretta3x@gmail.com>
2012-11-21 01:20:34 -0200

    Adicionada classe generate burndown

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('doctrine', 'utils', 'session', 'dateutil');
+$autoload['libraries'] = array('doctrine', 'utils', 'session', 'dateutil', 'generateburndown');
 
 
 /*
