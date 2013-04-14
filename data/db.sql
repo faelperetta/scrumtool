dcd1036590794c4d7d5c2bfc64da70ddcd1c68d9
Rafael Peretta Santos <peretta3x@gmail.com>
2013-04-14 16:18:36 -0300

    Atualização do scrio de criação da base de dados.

--- data/db.sql
+++ data/db.sql
@@ -1,162 +1,156 @@
-CREATE DATABASE  IF NOT EXISTS `scrumtool` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `scrumtool`;
--- MySQL dump 10.13  Distrib 5.5.16, for Win32 (x86)
---
--- Host: localhost    Database: scrumtool
--- ------------------------------------------------------
--- Server version	5.5.23
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE SCHEMA IF NOT EXISTS `scrumtool` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+USE `scrumtool` ;
 
---
--- Table structure for table `category`
---
+-- -----------------------------------------------------
+-- Table `scrumtool`.`project`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `scrumtool`.`project` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(45) NOT NULL ,
+  `description` VARCHAR(255) NULL ,
+  `create_at` TIMESTAMP NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `category`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `category` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `project`
---
+-- -----------------------------------------------------
+-- Table `scrumtool`.`user`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `scrumtool`.`user` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(45) NOT NULL ,
+  `email` VARCHAR(45) NOT NULL ,
+  `password` VARCHAR(40) NOT NULL ,
+  `role` VARCHAR(30) NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `project`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `project` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `story`
---
+-- -----------------------------------------------------
+-- Table `scrumtool`.`category`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `scrumtool`.`category` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(45) NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `story`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `story` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `description` text NOT NULL,
-  `status` varchar(30) NOT NULL,
-  `point` int(11) DEFAULT NULL,
-  `project_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `category_id` int(11) NOT NULL,
-  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `sprint_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_story_project` (`project_id`),
-  KEY `fk_story_user1` (`user_id`),
-  KEY `fk_story_category1` (`category_id`),
-  KEY `fk_story_sprint1` (`sprint_id`),
-  CONSTRAINT `fk_story_project` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_story_user1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_story_category1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_story_sprint1` FOREIGN KEY (`sprint_id`) REFERENCES `sprint` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `task`
---
+-- -----------------------------------------------------
+-- Table `scrumtool`.`sprint`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `scrumtool`.`sprint` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(45) NOT NULL ,
+  `start_date` DATETIME NULL ,
+  `end_date` DATETIME NULL ,
+  `project_id` INT NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_sprint_project1` (`project_id` ASC) ,
+  CONSTRAINT `fk_sprint_project1`
+    FOREIGN KEY (`project_id` )
+    REFERENCES `scrumtool`.`project` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `task`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `task` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `description` varchar(255) NOT NULL,
-  `hours` int(11) DEFAULT NULL,
-  `story_id` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `status` varchar(30) DEFAULT NULL,
-  `completed_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_task_story1` (`story_id`),
-  KEY `fk_task_user1` (`user_id`),
-  CONSTRAINT `fk_task_story1` FOREIGN KEY (`story_id`) REFERENCES `story` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_task_user1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `user`
---
+-- -----------------------------------------------------
+-- Table `scrumtool`.`story`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `scrumtool`.`story` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(45) NOT NULL ,
+  `description` TEXT NOT NULL ,
+  `status` VARCHAR(30) NOT NULL ,
+  `point` INT NULL ,
+  `project_id` INT NOT NULL ,
+  `user_id` INT NOT NULL ,
+  `category_id` INT NOT NULL ,
+  `create_at` TIMESTAMP NOT NULL ,
+  `sprint_id` INT NULL ,
+  `priority` INT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_story_project` (`project_id` ASC) ,
+  INDEX `fk_story_user1` (`user_id` ASC) ,
+  INDEX `fk_story_category1` (`category_id` ASC) ,
+  INDEX `fk_story_sprint1` (`sprint_id` ASC) ,
+  CONSTRAINT `fk_story_project`
+    FOREIGN KEY (`project_id` )
+    REFERENCES `scrumtool`.`project` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_story_user1`
+    FOREIGN KEY (`user_id` )
+    REFERENCES `scrumtool`.`user` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_story_category1`
+    FOREIGN KEY (`category_id` )
+    REFERENCES `scrumtool`.`category` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_story_sprint1`
+    FOREIGN KEY (`sprint_id` )
+    REFERENCES `scrumtool`.`sprint` (`id` )
+    ON DELETE SET NULL
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `password` varchar(40) NOT NULL,
-  `role` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `project_user`
---
+-- -----------------------------------------------------
+-- Table `scrumtool`.`task`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `scrumtool`.`task` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `description` VARCHAR(255) NOT NULL ,
+  `hours` INT NULL ,
+  `story_id` INT NOT NULL ,
+  `user_id` INT NULL ,
+  `status` VARCHAR(30) NULL ,
+  `completed_at` TIMESTAMP NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_task_story1` (`story_id` ASC) ,
+  INDEX `fk_task_user1` (`user_id` ASC) ,
+  CONSTRAINT `fk_task_story1`
+    FOREIGN KEY (`story_id` )
+    REFERENCES `scrumtool`.`story` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_task_user1`
+    FOREIGN KEY (`user_id` )
+    REFERENCES `scrumtool`.`user` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `project_user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `project_user` (
-  `project_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  PRIMARY KEY (`project_id`,`user_id`),
-  KEY `fk_user_id` (`user_id`),
-  CONSTRAINT `fk_project_id` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `sprint`
---
+-- -----------------------------------------------------
+-- Table `scrumtool`.`project_user`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `scrumtool`.`project_user` (
+  `project_id` INT NOT NULL ,
+  `user_id` INT NOT NULL ,
+  PRIMARY KEY (`project_id`, `user_id`) ,
+  INDEX `fk_user_id` (`user_id` ASC) ,
+  CONSTRAINT `fk_project_id`
+    FOREIGN KEY (`project_id` )
+    REFERENCES `scrumtool`.`project` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_id`
+    FOREIGN KEY (`user_id` )
+    REFERENCES `scrumtool`.`user` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `sprint`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `sprint` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `start_date` datetime DEFAULT NULL,
-  `end_date` datetime DEFAULT NULL,
-  `project_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_sprint_project1` (`project_id`),
-  CONSTRAINT `fk_sprint_project1` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
